17e24ecff3df181d1c3fa486281971d250b173c7
Athul Suresh <athulsure@gmail.com>
2017-09-14 13:03:34 +0530

    Clean up ADT. Add custom printing style

--- src/TigerParseHelper.hs
+++ src/TigerParseHelper.hs
@@ -1,9 +1,22 @@
-module TigerParseHelper where
+module TigerParseHelper (Program(..),
+                         Exp(..),
+                         LValue(..),
+                         InfixOp(..),
+                         FieldCreate(..),
+                         Decl(..),
+                         Type(..),
+                         FieldDecl(..),
+                        ) where
 
 type Id = String
 
 data Program = Program Exp
-             deriving (Show, Eq)
+             deriving (Eq)
+
+instance Show Program where
+        show (Program e) = "(Program " ++ show e ++ ")"
+
+
 
 data Exp = LExp LValue
          | NilValue
@@ -11,21 +24,38 @@ data Exp = LExp LValue
          | StringLiteral String
          | SeqExp [Exp]
          | Negation Exp
-         | CallExp { functionName :: Id, args :: [Exp] }
-         | InfixExp { lhs :: Exp, op :: InfixOp, rhs :: Exp }
+         | CallExp { callFunId :: Id, callFunArgs :: [Exp] }
+         | InfixExp { infixLhs :: Exp, op :: InfixOp, infixRhs :: Exp }
          | ArrCreate { arrType :: Id, size :: Exp, defVal :: Exp }
-         | RecCreate { recType :: Id, fields :: [FieldCreate] }
-         | Assignment { lhs :: LValue, rhs :: Exp }
-         | IfThen { condition :: Exp, thenExp :: Exp, elseExp :: Maybe Exp }
-         | WhileExp { condition :: Exp, body :: Exp }
-         | ForExp { loopVar :: Id, low :: Exp, high :: Exp, body :: Exp }
+         | RecCreate { recType :: Id, recFields :: [FieldCreate] }
+         | Assignment { assignmentLhs :: LValue, assignmentRhs :: Exp }
+         | IfThen { ifCond :: Exp, thenExp :: Exp, elseExp :: Maybe Exp }
+         | WhileExp { whileCond :: Exp, whileBody :: Exp }
+         | ForExp { forVar :: Id, low :: Exp, high :: Exp, forBody :: Exp }
          | Break
-         | LetExp { decl :: [Decl], body :: [Exp] }
-         deriving (Show, Eq)
+         | LetExp { letDecl :: [Decl], letBody :: [Exp] }
+         deriving (Eq)
 
+instance Show Exp where
+        show (LExp lv) = "(LExp " ++ show lv ++ ")"
+        show NilValue = "(NilValue)"
+        show (IntLiteral i) = "(IntLiteral " ++ show i ++ ")"
+        show (StringLiteral s) = "(StringLiteral " ++ show s ++ ")"
+        show (SeqExp xs) = "(SeqExp " ++ show xs ++ ")"
+        show (Negation e) = "(Negation " ++ show e ++ ")"
+        show (CallExp { callFunId = cfid, callFunArgs = cfargs }) = "(CallExp " ++ show cfid ++ " " ++ show cfargs ++ ")"
+        show (InfixExp { infixLhs = il, op = op, infixRhs = ir }) = "(InfixExp " ++ show il ++ " " ++ show op ++ " " ++ show ir ++ ")"
+        show (ArrCreate { arrType = at, size = sz, defVal = dv }) = "(ArrCreate " ++ show at ++ " " ++ show sz ++ " " ++ show dv ++ ")"
+        show (RecCreate { recType = rt, recFields = rf }) = "(RecCreate " ++ show rt ++ " " ++ show rf ++ ")"
+        show (Assignment { assignmentLhs = al, assignmentRhs = ar }) = "(Assignment " ++ show al ++ " " ++ show ar ++ ")"
+        show (IfThen { ifCond = ic, thenExp = te, elseExp = ee }) = "(IfThen " ++ show ic ++ " " ++ show te ++ " " ++ show ee ++ ")"
+        show (WhileExp { whileCond = wc, whileBody = wb }) = "(WhileExp " ++ show wc ++ " " ++ show wb ++ ")"
+        show (ForExp { forVar = fv, low = l, high = h, forBody = fb }) = "(ForExp " ++ show fv ++ " " ++ show l ++ " " ++ show h ++ " " ++ show fb ++ ")"
+        show (Break) = "(Break)"
+        show (LetExp { letDecl = ld, letBody = lb }) = "(LetExp " ++ show ld ++ " " ++ show lb ++ ")"
 
 data LValue = LVar Id
-            | LSubscript LVaule Exp
+            | LSubscript LValue Exp
             | LField LValue Id
             deriving (Show, Eq)
 
@@ -44,15 +74,17 @@ data InfixOp = Add
 data FieldCreate = FieldCreate Id Exp
                  deriving (Show, Eq) 
 
-data Decl = TypeDec { typeId :: Id, type :: Type }
+data Decl = TypeDec { typeId :: Id, ty :: Type }
           | VarDec  { varId :: Id, varType :: Maybe Id, value :: Exp }
-          | FunDec  { funId :: Id, args :: [FieldDec], funRetType :: Maybe Id, funDef :: Exp }
+          | FunDec  { declFunId :: Id, declFunArgs :: [FieldDecl], funRetType :: Maybe Id, funDef :: Exp }
           deriving (Show, Eq)
 
 data Type = Type Id
           | ArrType Id
-          | RecType FieldDec
+          | RecType FieldDecl
           deriving (Show, Eq)
 
-data FieldDec = FieldDec { fId :: Id, fType :: Id }
-              deriving (Show, Eq)
+data FieldDecl = FieldDecl { fId :: Id, fType :: Id }
+               deriving (Show, Eq)
+
+-- Helper functions for displaying the tree
